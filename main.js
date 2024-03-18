c931911deb0437d3ebddeb6d6106e2e5e615f42c
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2024-03-18 12:23:15 -0700

    Update main.js
    codewars 3/18

--- main.js
+++ main.js
@@ -3,3 +3,13 @@
 function descendingOrder(n){
   return parseInt(String(n).split('').sort().reverse().join(''))
 }
+
+
+//Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+//For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+function sumTwoSmallestNumbers(numbers) {  
+  //Code here
+  let newArr = numbers.sort((a, b) => a - b)
+  return newArr[0] + newArr[1]
+}
