4e1ab4a364a9e09c0fa24e7538f86cde752c1474
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2024-03-21 11:40:24 -0700

    Update main.js
    3/21

--- main.js
+++ main.js
@@ -61,3 +61,19 @@ function high(x){
   
   return highestScoreWord;
 }
+
+
+// 3.21
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+
+const expandedForm = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map( (a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");
