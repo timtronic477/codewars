ec78f3608d7fc3e5eaadfb4edd02aae263b22f11
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2024-03-22 13:52:07 -0700

    Update main.js
    3.22

--- main.js
+++ main.js
@@ -101,3 +101,18 @@ function alphabetPosition(text) {
 
   return result.slice(0, result.length-1);
 }
+
+// 3.22 8
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+// Examples
+// n = 0  ==> [1]        # [2^0]
+// n = 1  ==> [1, 2]     # [2^0, 2^1]
+// n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+function powersOfTwo(n){
+  let totals = [];
+  for (let i = 0; i <=n; i++){
+    totals.push(2 ** i)
+  }
+  return totals
+}
