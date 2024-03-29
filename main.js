f724b9e785109e02d32235441847fec7994049be
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2024-03-29 13:04:45 -0700

    Update main.js
    3.29

--- main.js
+++ main.js
@@ -184,3 +184,18 @@ function points(games) {
   })  
   return score
 }
+
+
+// 3.29 level 6
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count (string) {  
+  var count = {};
+  string.split('').forEach((s) => {
+     count[s] ? count[s]++ : count[s] = 1;
+  });
+  return count;
+}
+
+
