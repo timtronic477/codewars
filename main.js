bc32c5c444f73a552236750c0a6b159deb132c31
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2024-04-01 11:44:08 -0700

    Update main.js
    4.1

--- main.js
+++ main.js
@@ -199,3 +199,27 @@ function count (string) {
 }
 
 
+// 4.1 level 7
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+
+function XO(str) {
+    //code here
+  let x = 0;
+  let o = 0;
+  for(let i = 0; i<str.length; i++){
+    if(str[i].toLowerCase() === "x") {
+      x++
+    } else if (str[i].toLowerCase() === "o"){
+      o++
+    }
+  }
+  return x === o
+}
