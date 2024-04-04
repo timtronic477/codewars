d43e28ce3dca51c76ae138294646392843022c65
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2024-04-03 19:20:55 -0700

    Update main.js
    4.3

--- main.js
+++ main.js
@@ -242,3 +242,32 @@ function persistence(num) {
   }
   return output
 }
+
+// 4.3 level 6
+// Build Tower
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+// For example, a tower with 3 floors looks like this:
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+
+function towerBuilder(nFloors) {
+  // build here
+  var tower = [];
+  for (var i = 0; i < nFloors; i++){
+    tower.push(" ".repeat(nFloors - i -1) + "*".repeat((i*2) +1) + " ".repeat(nFloors - i -1));
+      
+  }
+  return tower
+}
