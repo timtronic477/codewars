f69ef4087de0231e21a87ef946812ac74f2b3255
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2024-04-10 15:28:56 -0700

    Create Double Char.py
    4.10

---
+++ Double Char.py
@@ -0,0 +1,12 @@
+# Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+# Examples (Input -> Output):
+# * "String"      -> "SSttrriinngg"
+# * "Hello World" -> "HHeelllloo  WWoorrlldd"
+# * "1234!_ "     -> "11223344!!__  "
+
+def double_char(s):
+    output = ''
+    for i in s:
+        output += i*2
+    return output
