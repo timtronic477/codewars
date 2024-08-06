842964a120a0811e68eaf943ccad424e762482b0
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2024-08-06 11:28:04 -0700

    Create get character from ASCII Value.py
    8.6

---
+++ get character from ASCII Value.py
@@ -0,0 +1,10 @@
+# Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+# Example:
+
+# 65 --> 'A'
+# 97 --> 'a'
+# 48 --> '0
+
+def get_char(c):
+    return chr(c)
