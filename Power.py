ff814dfebb7558f61748f0a376db88dbdb5a813a
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2024-11-28 00:19:42 -0800

    Create Power.py
    11.28

---
+++ Power.py
@@ -0,0 +1,16 @@
+# The goal is to create a function of two inputs number and power, that "raises" the number up to power (ie multiplies number by itself power times).
+# Examples
+
+# number_to_pwr(3, 2)  # -> 9 ( = 3 * 3 )
+# number_to_pwr(2, 3)  # -> 8 ( = 2 * 2 * 2 )
+# number_to_pwr(10, 6) # -> 1000000
+
+# Note: math.pow and some others math functions are disabled on final tests.
+
+def number_to_pwr(number, p): 
+    if p == 0: return 1
+    total = number
+    for i in range(1, p):
+        total *= number
+    return total
+
