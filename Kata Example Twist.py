620c7b2d8b2d305d37e41ad010a444a736ad5b2c
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2024-12-16 22:44:44 -0800

    Create Kata Example Twist.py
    12.16

---
+++ Kata Example Twist.py
@@ -0,0 +1,8 @@
+# This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+# Add the value "codewars" to the array websites 1,000 times.
+
+websites = []
+
+for i in range(0, 1000):
+    websites.append('codewars')
