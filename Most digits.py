c40424bd051ebec7f32e53d6534972f9498cd05c
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2024-12-17 21:27:07 -0800

    Update Most digits.py
    fixed buggy answer

--- Most digits.py
+++ Most digits.py
@@ -3,4 +3,8 @@
 # If two numbers in the argument array have the same number of digits, return the first one in the array.
 
 def find_longest(arr):
-    return list(sorted(arr))[len(arr)-1]
+    longest = 0
+    for i in arr:
+        if len(list(str(i))) > len(list(str(longest))):
+            longest = i
+    return longest
