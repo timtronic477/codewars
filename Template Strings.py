88fa2a64778e38dc9e844437e0f809d9fe39f55f
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2025-01-12 21:57:04 -0800

    Create Template Strings.py
    1.12

---
+++ Template Strings.py
@@ -0,0 +1,10 @@
+# Template Strings
+# Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+# Task
+# Your task is to return the correct string using the Template String Feature.
+# Input
+# Two Strings, no validation is needed.
+# Output
+
+def temple_strings(obj, feature): 
+    return obj + ' are ' + feature
