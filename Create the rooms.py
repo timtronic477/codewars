b85a7f7529e9312ff1e21c05619839ff8eab2deb
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2025-02-01 23:38:52 -0800

    Create Create the rooms.py
    2.1

---
+++ Create the rooms.py
@@ -0,0 +1,5 @@
+# Escape the room
+
+# You are creating an "Escape the room" game. The first step is to create a hash table ( dict in Python) called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each room being a hash table with at least 3 properties (e.g. name, description, completed
+
+rooms = {"room1":{"name": 'room1', "description": 'first room', "completed": True}, "room2":{"name": 'room2', "description": 'second room', "completed": True}, "room3":{"name": 'room3', "description": 'third room', "completed": True}}
