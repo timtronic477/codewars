906fbe5ebb1c687baf96f87dcae4ed23eec9b484
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2025-03-22 16:55:52 -0700

    Create Apple Turnover.py
    3.22

---
+++ Apple Turnover.py
@@ -0,0 +1,6 @@
+# Alan is known for referring to the temperature of the apple turnover as Hotter than the sun!. According to space.com the temperature of the sun's corona is 2,000,000 degrees Celsius, but we will ignore the science for now.
+# Task
+
+# Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+def apple(x):
+    return "It's hotter than the sun!!" if int(x)**2 > 1000 else "Help yourself to a honeycomb Yorkie for the glovebox."
