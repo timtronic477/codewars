7adbab67ae5186df9510307a8de9b2463566584d
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2025-06-27 11:12:56 -0700

    Create Simple beads count.py
    6.27

---
+++ Simple beads count.py
@@ -0,0 +1,9 @@
+# Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+# @ @@ @ @@ @ @@ @ @@ @ @@ @
+
+# Implement a function returning the number of red beads.
+# If there are less than 2 blue beads return 0. 
+
+def count_red_beads(n):
+    return (n-1)*2
