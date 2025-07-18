dc30ea1750f2dc821dba3dc97ab27f59b94d496e
Timothy Balilo <85888459+timtronic477@users.noreply.github.com>
2025-07-17 22:09:21 -0700

    Create  Sum of all arguments.py
    7.17

---
+++ Sum of all arguments.py
@@ -0,0 +1,9 @@
+# Write a function that finds the sum of all its arguments.
+
+# eg:
+
+# sum_args(1, 2, 3) # => 6
+# sum_args(8, 2) # => 10
+# sum_args(1, 2, 3, 4, 5) # => 15
+def sum_args(*args):
+    return sum(list(args))
